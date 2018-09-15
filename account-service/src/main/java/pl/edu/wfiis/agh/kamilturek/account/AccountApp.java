f3b2c56d64e9d9edd9625d949caefbae084988d4
Peteef <kamilptf@gmail.com>
2018-09-16 01:43:10 +0200

    Adding some raw services

---
+++ account-service/src/main/java/pl/edu/wfiis/agh/kamilturek/account/AccountApp.java
@@ -0,0 +1,28 @@
+package pl.edu.wfiis.agh.kamilturek.account;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class AccountApp {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(AccountApp.class, args);
+        System.out.println(context.getBean(A.class));
+    }
+
+    @Component
+    class A {
+        @Value("${author.firstname}")
+        private String name;
+
+        @Override
+        public String toString() {
+            return "A{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+    }
+}
