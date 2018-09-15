03aa3eee41b8c57baf17051f0c187f72b1b94a1f
Peteef <kamilptf@gmail.com>
2018-09-16 00:54:45 +0200

    Application redesign

---
+++ config-server/src/main/java/pl/edu/agh/wfiis/kamilturek/configserver/config/ConfigServerConfiguration.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.wfiis.kamilturek.configserver.config;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigServer
+public class ConfigServerConfiguration {
+}
