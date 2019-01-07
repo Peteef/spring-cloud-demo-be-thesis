7ee044688214c8766a7e8a2509c0385f6df52137
Peteef <kamilptf@gmail.com>
2019-01-07 01:21:14 +0100

    Implementing Spring Cloud Eureka

---
+++ discovery-service/src/main/java/pl/edu/agh/wfiis/kamilturek/discoveryservice/DiscoveryServiceApp.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.wfiis.kamilturek.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApp.class, args);
+    }
+}
