35e35c4471256593bd843ce1e26a84b4676a190a
Peteef <kamilptf@gmail.com>
2019-01-11 01:01:29 +0100

    Implementation of Gateway Service

---
+++ gateway-service/src/main/java/pl/edu/agh/wfiis/kamilturek/gatewayservice/GatewayServiceApp.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.wfiis.kamilturek.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApp.class, args);
+    }
+}
---
+++ gateway-service/src/main/java/pl/edu/agh/wfiis/kamilturek/gatewayservice/config/GatewayServiceConfiguration.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.wfiis.kamilturek.gatewayservice.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class GatewayServiceConfiguration {
+}
